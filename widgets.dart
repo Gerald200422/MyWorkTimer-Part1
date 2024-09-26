5e09c73d2fe3f831b29ceaf3b5dba2df47a76ea2
Gerald P. Balete <geraldbalete123@gmail.com>
2024-09-26 13:35:32 +0800

    Add files via upload

---
+++ widgets.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ProductivityButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final double size;
+  final VoidCallback onPressed;
+
+  const ProductivityButton({
+    Key? key,
+    required this.color,
+    required this.text,
+    required this.onPressed,
+    required this.size,
+
+  }) : super(key:key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialButton(
+      child: Text(
+        text,
+        style: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+      ),
+
+      onPressed: onPressed,
+    color: color,
+    minWidth: size,
+    );
+  }
+}
